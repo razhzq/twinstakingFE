a3e0cb8907e70fd6888502ab32a19f29440efcdc
accompany1205 <accompaniedwithyou@gmail.com>
2023-02-23 16:09:36 +0900

    feat: heroapp changed

--- src/partials/apps/HeroApp copy.jsx
+++ src/partials/apps/HeroApp copy.jsx
@@ -103,7 +103,7 @@ function HeroApp() {
     <section className="px-4 md:px-16 lg:px-36">
       <div className="max-w-6xl relative">
         {/* Hero content */}
-        <div className="relative pt-32 pb-5 md:pt-40 md:pb-16 ">
+        <div className="relative pt-32 pb-5 md:pt-40 md:pb-16">
           {/* Section header */}
           <div className="max-w-2xl pb-0 md:pb-16">
             <p className="mb-8 text-5xl font-black" style={{ fontFamily: 'system-ui' }} data-aos="fade-up">
@@ -112,30 +112,75 @@ function HeroApp() {
               <span className="text-stroke"> STAKING</span>
             </p>
             
-            <div className="w-full px-8 py-8 text-gray-100 border rounded-3xl bg-[#253051]">
-              <div className="flex justify-between text-xs">
-                <span>PRICE PER DISACART</span>
-                <span>TOTAL</span>
+            <div className="w-full bg-slate-500 text-gray-100 border rounded-xl">
+              <p className="text-2xl px-6 py-6 font-bold">Twin Staking Pool</p>
+              <div className="grid grid-cols-1 divide-y bg-gray-100 text-gray-900">
+                {STAKING_LIST.map((item, index) => (
+                  <div key={index} className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>{item.title}</div>
+                    <div>{item.value}</div>
+                  </div>
+                ))}
+                  <div  className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>Maturity</div>
+                    <div>{maturity}{" "}Days</div>
+                  </div>
+                  <div className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>TVL</div>
+                    <div>{total}{" "}TWIN</div>
+                  </div>
+                  <div className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>Pool Status</div>
+                    <div>{poolStatus}</div>
+                  </div>
+                  <div className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>Your Stake</div>
+                    <div>{stakedAmount}{" "}TWIN</div>
+                  </div>
+                  {/* <div className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>TWIN Balance</div>
+                    <div>{balance}</div>
+                  </div> */}
+                  <div className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>TWIN Accumulated Rewards</div>
+                    <div>{reward}</div>
+                  </div>
+                  <div className="flex justify-between px-6 md:px-10 py-4 text-md font-normal">
+                    <div>TWIN Claimable Rewards</div>
+                    <div>{reward}</div>
+                  </div>
               </div>
-              <div className="flex justify-between text-xl font-bold">
-                <span>-BNB</span>
-                <span>-BNB</span>
-              </div>
-              <hr className='w-full h-1 bg-white'/>
-              <div>
-                <p className='my-8 text-center text-xl font-bold'>MINT YOUR</p>
-                <h1 className='mb-8 text-center text-3xl font-bold text-[#89a7e0]'>Dosacraf</h1>
-                <div className="flex items-center mb-8">
-                  <div className="flex justify-center items-center w-16 h-10 bg-[#89a7e0] text-white font-bold rounded-l-3xl cursor-pointer">-</div>
-                  <div className="flex justify-center items-center w-full h-10 bg-white text-black font-extrabold text-xl">1</div>
-                  <div className="flex justify-center items-center w-16 h-10 bg-[#89a7e0] text-white font-bold rounded-r-3xl cursor-pointer">+</div>
+            
+              <div className="bg-gray-100 pt-4">
+                <Link to="https://oreoswap.finance/swap?outputCurrency=0x14917033ec93bc13bf9e41427fab7ed56dc64349" className="block w-full mb-6 text-center text-gray-900 underline" aria-label="Cruip">
+                  Get TWIN
+                </Link>
+                <div className="md:flex justify-around pb-8 px-2">
+                  <button className="btn-twin w-full text-white text-center font-semibold round-md py-3 px-8 sm:w-auto mb-2 sm:mb-0" href="#0"
+                   onClick={() => {
+                    setShow(true);
+                    console.log(show)
+                   }}
+                  >
+                    Stake
+                  </button>
+                  <button className="btn-twin w-full text-white text-center font-semibold round-md py-3 px-8 sm:w-auto mb-2 sm:mb-0" href="#0"
+                    onClick={() => unstake()}
+                  >
+                    Unstake
+                  </button>
+                  <button className="btn-twin w-full text-white text-center font-semibold round-md py-3 px-8 sm:w-auto mb-2 sm:mb-0" href="#0"
+                    onClick={() => {
+                      if(reward != 0) {
+                        claim();
+                      } else {
+                        alert("No reward to claim!");
+                      }
+                    }}
+                  >
+                    Claim
+                  </button>
                 </div>
-              <hr className='w-full h-1 bg-white'/>
-              <div className="pt-8">
-                <p className='text-center text-md font-bold'>TOTAL MINTED -</p>
-                <div className="flex justify-center items-center w-full h-10 my-8 bg-[#89a7e0] text-gray-300 rounded-3xl"> MINT </div>
-                <p className='text-center text-md font-bold'>FREEBIES 0</p>
-              </div>
               </div>
             </div>
           </div>
